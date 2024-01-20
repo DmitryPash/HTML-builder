095e2bb1a39292aa02a938a9ddf8b9fd89945f79
Dmitry <fomnq9@gmail.com>
2024-01-20 10:40:47 +0300

    dqw
    dqw

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, '/text.txt'), 'utf8', function(error, fileContent){
+   if(error) throw error; 
+   console.log(fileContent);
+});
